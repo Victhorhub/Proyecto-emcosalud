b8ef972ff8ddfc70cd6ce21742c596243ac0226f
TheReDNooB <xmiguelx209@gmail.com>
2024-06-23 18:18:12 -0500

    show the data

--- EmcoCrud/core/views.py
+++ EmcoCrud/core/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Funtionary
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "core/base.html")
+    funtionary_ = Funtionary.objects.all()
+    return render(request, "core/home.html", {"funtionary_" : funtionary_})
 
 def update_info(request):
     return render(request, "core/update_info.html")
