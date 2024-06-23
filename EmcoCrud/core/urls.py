d7635d9110ef3b95466ac5fc7e3511cbec4b274a
TheReDNooB <xmiguelx209@gmail.com>
2024-06-23 11:02:06 -0500

    Intruce to the templates

--- EmcoCrud/core/urls.py
+++ EmcoCrud/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.helloworld),
-    path('update_info', views.update_info),
-    path('about', views.about),
+    path('', views.helloworld, name="home"),
+    path('update_info', views.update_info, name="update_info"),
+    path('about', views.about, name="about"),
 ]
