0730200017664aa147107a23966b68be0c360a7c
TheReDNooB <xmiguelx209@gmail.com>
2024-06-23 13:13:55 -0500

    updating of models

--- EmcoCrud/core/models.py
+++ EmcoCrud/core/models.py
@@ -15,3 +15,31 @@ class Country(models.Model):
 
     def __str__(self):
         return self.nameCountry
+
+class State(models.Model):
+    nameState = models.CharField(max_length=200, verbose_name="Nombre del estado")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edicion")
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Estados/Departamentos"
+        verbose_name_plural = "Estado/Departamentos"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.nameState
+
+class City(models.Model):
+    nameCity = models.CharField(max_length=200, verbose_name="Nombre de la ciudad")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edicion")
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Ciudad"
+        verbose_name_plural = "Ciudades"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.nameCity
