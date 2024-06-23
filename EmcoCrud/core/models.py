c5fe1151f14365436734d489808f302d50a67ecf
TheReDNooB <xmiguelx209@gmail.com>
2024-06-23 11:42:52 -0500

    I create the models for the countries

--- EmcoCrud/core/models.py
+++ EmcoCrud/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Country(models.Model):
+    nameCountry = models.CharField(max_length=200, verbose_name="Nombre del pais")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edicion")
+
+    class Meta:
+        verbose_name = "Pais"
+        verbose_name_plural = "Paises"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.nameCountry
--- EmcoCrud/core/urls.py
+++ EmcoCrud/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.helloworld, name="home"),
+    path('', views.home, name="home"),
     path('update_info', views.update_info, name="update_info"),
     path('about', views.about, name="about"),
 ]
