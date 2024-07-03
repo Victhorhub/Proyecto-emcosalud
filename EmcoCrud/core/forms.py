d970ca3c5e34a87ead10608a57df13b3f045240c
TheReDNooB <xmiguelx209@gmail.com>
2024-07-02 20:41:52 -0500

    adjusting the form from the view

--- EmcoCrud/core/forms.py
+++ EmcoCrud/core/forms.py
@@ -6,5 +6,5 @@ class FuntionaryForm(forms.ModelForm):
     class Meta:
         model = Funtionary
         fields = ("identification", "name", "lastname", "birthday", "telephone", "email", "active"
-        , "speciality", "curriculum", "diploma", "Retus", "courses", "degree", "title", "contract"
+        , "speciality", "curriculum", "diploma", "Retus", "courses", "degree_certificate", "title", "contract"
         , "type_vinculation", "policy", "tariff", "Headquarters")
--- EmcoCrud/core/views.py
+++ EmcoCrud/core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Funtionary
 from core.forms import FuntionaryForm
+from django.views.generic import CreateView
+from django.views.generic import TemplateView
 
 # Create your views here.
 
@@ -16,5 +18,42 @@ def update_info(request):
 def about(request):
     return render(request, "core/about.html")
 
-def add_data(request):
-    return render(request, "core/add_data.html")
+
+class RegisterData_View(CreateView):
+    form_class = FuntionaryForm
+    template_name = "core/add_data.html"
+
+    def get_form(self, form_class=None):
+        form = super (RegisterData_View, self).get_form()
+        form.fields['identification'].widget = forms.IntegerField( attrs = {'class': 'form-control mb-2', "placeholder": 'Identificacion'})
+        form.fields['name'].widget = forms.CharField( attrs = {'class': 'form-control mb-2', "placeholder": 'Nombre'})
+        form.fields['lastname'].widget = forms.CharField( attrs = {'class': 'form-control mb-2', "placeholder": 'Apellido'})
+        form.fields['birthday'].widget = forms.DateField( attrs = {'class': 'form-control mb-2', "placeholder": 'Fecha de Nacimiento'})
+        form.fields['telephone'].widget = forms.CharField( attrs = {'class': 'form-control mb-2', "placeholder": 'Telefono'})
+        form.fields['EmailField'].widget = forms.EmailField( attrs = {'class': 'form-control mb-2', "placeholder": 'Email'})
+        form.fields['active'].widget = forms.IntegerField( attrs = {'class': 'form-control mb-2'})
+        form.fields['BooleanField'].widget = forms.BooleanField( attrs = {'class': 'form-control mb-2'})
+        form.fields['speciality'].widget = forms.ForeignKey( attrs = {'class="form-select"'})
+        form.fields['curriculum'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['diploma'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['Retus'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['courses'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['degree_certificate'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['title'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['contract'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['policy'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['tariff'].widget = forms.IntegerField( attrs = {'class': 'form-control mb-2', "placeholder": 'Tarifa'})
+        form.fields['Headquarters'].widget = forms.ForeignKey( attrs = {'class="form-select" multiple aria-label="Multiple select example"'})
+
+
+"""
+
+"", "", "", "", "", "", ""
+        , "", "", "", "", "", "", "", ""
+        , "", "", "", ""
+
+"""
+
+# def add_data(request):
+#     context = {'form': FuntionaryForm()}
+#     return render(request, "core/add_data.html", context)
