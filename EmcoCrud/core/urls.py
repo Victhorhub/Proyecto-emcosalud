7cb3d1cf3c85804334829b3f3003714ce82245fc
TheReDNooB <xmiguelx209@gmail.com>
2024-07-02 21:52:02 -0500

    render the funtionary form

--- EmcoCrud/core/urls.py
+++ EmcoCrud/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from core.views import RegisterData_View
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('update_info', views.update_info, name="update_info"),
     path('about', views.about, name="about"),
-    path('add_data', views.add_data, name="add_data"),
+    path('register_data', RegisterData_View.as_view(), name="add_data"),
 ]
--- EmcoCrud/core/views.py
+++ EmcoCrud/core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Funtionary
-from core.forms import FuntionaryForm
+from  django import forms
+from .forms import FuntionaryForm
 from django.views.generic import CreateView
 from django.views.generic import TemplateView
 
@@ -25,35 +26,41 @@ class RegisterData_View(CreateView):
 
     def get_form(self, form_class=None):
         form = super (RegisterData_View, self).get_form()
-        form.fields['identification'].widget = forms.IntegerField( attrs = {'class': 'form-control mb-2', "placeholder": 'Identificacion'})
-        form.fields['name'].widget = forms.CharField( attrs = {'class': 'form-control mb-2', "placeholder": 'Nombre'})
-        form.fields['lastname'].widget = forms.CharField( attrs = {'class': 'form-control mb-2', "placeholder": 'Apellido'})
-        form.fields['birthday'].widget = forms.DateField( attrs = {'class': 'form-control mb-2', "placeholder": 'Fecha de Nacimiento'})
-        form.fields['telephone'].widget = forms.CharField( attrs = {'class': 'form-control mb-2', "placeholder": 'Telefono'})
-        form.fields['EmailField'].widget = forms.EmailField( attrs = {'class': 'form-control mb-2', "placeholder": 'Email'})
-        form.fields['active'].widget = forms.IntegerField( attrs = {'class': 'form-control mb-2'})
-        form.fields['BooleanField'].widget = forms.BooleanField( attrs = {'class': 'form-control mb-2'})
-        form.fields['speciality'].widget = forms.ForeignKey( attrs = {'class="form-select"'})
-        form.fields['curriculum'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
-        form.fields['diploma'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
-        form.fields['Retus'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
-        form.fields['courses'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
-        form.fields['degree_certificate'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
-        form.fields['title'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
-        form.fields['contract'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
-        form.fields['policy'].widget = forms.FileField( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
-        form.fields['tariff'].widget = forms.IntegerField( attrs = {'class': 'form-control mb-2', "placeholder": 'Tarifa'})
-        form.fields['Headquarters'].widget = forms.ForeignKey( attrs = {'class="form-select" multiple aria-label="Multiple select example"'})
-
-
-"""
-
-"", "", "", "", "", "", ""
-        , "", "", "", "", "", "", "", ""
-        , "", "", "", ""
-
-"""
-
-# def add_data(request):
-#     context = {'form': FuntionaryForm()}
-#     return render(request, "core/add_data.html", context)
+        form.fields['identification'].widget = forms.NumberInput( attrs = {'class': 'form-control mb-2', "placeholder": 'Identificacion'})
+        form.fields['name'].widget = forms.TextInput( attrs = {'class': 'form-control mb-2', "placeholder": 'Nombre'})
+        form.fields['lastname'].widget = forms.TextInput( attrs = {'class': 'form-control mb-2', "placeholder": 'Apellido'})
+        form.fields['birthday'].widget = forms.DateInput( attrs = {'class': 'form-control mb-2', "placeholder": 'Fecha de Nacimiento'})
+        form.fields['telephone'].widget = forms.TextInput( attrs = {'class': 'form-control mb-2', "placeholder": 'Telefono'})
+        form.fields['email'].widget = forms.EmailInput( attrs = {'class': 'form-control mb-2', "placeholder": 'Email'})
+        form.fields['active'].widget = forms.CheckboxInput( attrs = {'class': 'form-control mb-2'})
+        form.fields['speciality'].widget = forms.Select( attrs = {'class':'form-select'})
+        form.fields['curriculum'].widget = forms.FileInput( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['diploma'].widget = forms.FileInput( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['Retus'].widget = forms.FileInput( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['courses'].widget = forms.FileInput( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['degree_certificate'].widget = forms.FileInput( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['title'].widget = forms.FileInput( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['contract'].widget = forms.FileInput( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['policy'].widget = forms.FileInput( attrs = {'class': 'form-control mb-2', "type": 'file', 'id': 'formFile'})
+        form.fields['tariff'].widget = forms.NumberInput( attrs = {'class': 'form-control mb-2', "placeholder": 'Tarifa'})
+        form.fields['Headquarters'].widget = forms.SelectMultiple( attrs = {'class': 'form-select' , 'multiple aria-label': 'Multiple select example'})
+        #Labels
+        form.fields['identification'].label = 'Identificacion'
+        form.fields['name'].label = 'Nombre'
+        form.fields['lastname'].label = 'Apellido'
+        form.fields['birthday'].label = 'Fecha de nacimiento'
+        form.fields['telephone'].label = 'telefono'
+        form.fields['email'].label = 'Correo electronico'
+        form.fields['active'].label = 'Estado'
+        form.fields['speciality'].label = 'Especialidad'
+        form.fields['curriculum'].label = 'Hoja de Vida'
+        form.fields['diploma'].label = 'Diploma'
+        form.fields['Retus'].label = 'Retus'
+        form.fields['courses'].label = 'Cursos'
+        form.fields['degree_certificate'].label = 'Certificado de grado'
+        form.fields['title'].label = 'Titulo'
+        form.fields['contract'].label = 'Contrato'
+        form.fields['policy'].label = 'Poliza'
+        form.fields['tariff'].label = 'Tarifa'
+        form.fields['Headquarters'].label = 'Sede'
+        return form
