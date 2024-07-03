ad011b46172c7e7db417b3823a41443e01a05986
TheReDNooB <xmiguelx209@gmail.com>
2024-07-02 19:40:45 -0500

    front-end form

---
+++ EmcoCrud/core/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from core.models import Funtionary
+
+
+class FuntionaryForm(forms.ModelForm):
+    class Meta:
+        model = Funtionary
+        fields = ("identification", "name", "lastname", "birthday", "telephone", "email", "active"
+        , "speciality", "curriculum", "diploma", "Retus", "courses", "degree", "title", "contract"
+        , "type_vinculation", "policy", "tariff", "Headquarters")
