6e0f38b54f7166daea39455b223f003c1a765560
TheReDNooB <xmiguelx209@gmail.com>
2024-07-07 18:56:13 -0500

    render the multiple select for the headquarters

--- EmcoCrud/core/forms.py
+++ EmcoCrud/core/forms.py
@@ -1,10 +1,15 @@
 from django import forms
-from core.models import Funtionary
+from core.models import Funtionary, Headquarters
 
 
 class FuntionaryForm(forms.ModelForm):
+    funtionary_headquarters = forms.ModelMultipleChoiceField(
+        queryset=Headquarters.objects.all(),
+        widget=forms.SelectMultiple,
+    )
+    
     class Meta:
         model = Funtionary
         fields = ("identification", "name", "lastname", "birthday", "telephone", "email", "active"
         , "speciality", "curriculum", "diploma", "Retus", "courses", "degree_certificate", "title", "contract"
-        , "type_vinculation", "policy", "tariff", "Headquarters")
+        , "type_vinculation", "policy", "tariff", "funtionary_headquarters")
--- EmcoCrud/core/urls.py
+++ EmcoCrud/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from core.views import RegisterData_View
+#from core.views import RegisterData_View
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('update_info', views.update_info, name="update_info"),
     path('about', views.about, name="about"),
-    path('register_data', RegisterData_View.as_view(), name="add_data"),
+    #path('register_data', RegisterData_View.as_view(), name="add_data"),
+    path('upload_funtionary', views.upload_funtionary, name="upload_funtionary"),
 ]
