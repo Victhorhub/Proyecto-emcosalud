9fa48cb7b9e63dc28566bd1f6c2ac71c5640538d
TheReDNooB <xmiguelx209@gmail.com>
2024-07-14 22:26:06 -0500

    futures filters

--- EmcoCrud/core/views.py
+++ EmcoCrud/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from .models import Funtionary
+from .models import Funtionary, Speciality, Headquarters
 from  django import forms
 from .forms import FuntionaryForm
 from django.views.generic import CreateView
@@ -18,6 +18,26 @@ def search(request):
         funtionary_query = Funtionary.objects.all()
     context = {
         'funtionary_query' : funtionary_query
+    }
+    return render(request, "core/search.html", context)
+
+def filter_query(request):
+    funtionary_ = Funtionary.objects.all()
+    speciality_filter = Speciality.objects.all()
+    headquarters_filter = Headquarters.objects.all()
+    filter_speciality = request.GET.get('filter-speciality')
+    filter_headquarter = request.GET.get('filter-headquarter')
+
+    if is_valid_queryparam(filter_headquarter) and filter_headquarter != 'Choose...':
+        funtionary_ = funtionary_.filter(headquarters_filter__nameHeadquarter=filter_headquarter)
+
+    if is_valid_queryparam(filter_speciality) and filter_speciality != 'Choose...':
+        funtionary_ = funtionary_.filter(speciality_filter__title=filter_speciality)
+
+    context = {
+        'funtionary_query' : funtionary_query,
+        'speciality_filter' : speciality_filter,
+        'headquarters_filter' : headquarters_filter,
     }
     return render(request, "core/search.html", context)
 
